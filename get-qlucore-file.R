15f758485e98e494667a13b54793634adb6721db
Gig77 <frech.christian@gmail.com>
2016-05-27 09:24:47 +0200

    add GO terms to variable annotation

--- get-qlucore-file.R
+++ get-qlucore-file.R
@@ -11,10 +11,27 @@ for(g in groups$ID) {
   ann.sample[,g] <- ifelse(ann.sample$Alias %in% members, "yes", "no")
 }
 
+# add collapsed GO terms to variable annotation
+ensembl2go.file <- "/mnt/projects/schwann/results/ensembl2go.RData"
+if(file.exists(ensembl2go.file)) {
+  load(ensembl2go.file)
+} else {
+  library(biomaRt)
+  mart <- useMart(biomart="ENSEMBL_MART_ENSEMBL", host="feb2014.archive.ensembl.org", path="/biomart/martservice" ,dataset="hsapiens_gene_ensembl")
+  ensembl2go <- getBM(attributes = c("ensembl_gene_id", "go_id", "name_1006", "definition_1006", "go_linkage_type"), mart = mart)
+  save(ensembl2go, file=ensembl2go.file)
+}
+ensembl2go <- ensembl2go[ensembl2go$go_id != "",]
+ensembl2go$id_term <- paste(ensembl2go$go_id, ensembl2go$name_1006)
+ensembl2go.agg <- ensembl2go[,c("ensembl_gene_id", "id_term")]
+ensembl2go.agg <- aggregate(id_term~ensembl_gene_id, paste, collapse=", ", data=ensembl2go.agg)
+names(ensembl2go.agg) <- c("Ensembl Gene ID", "GO")
+ann.var <- merge(ann.var, ensembl2go.agg, all.x = T)
+ann.var.names <- c("Ensembl", "Description", "Band", "HGNC", "Chr", "Start", "End", "GO")
+names(ann.var) <- ann.var.names
+
 # annotate expression matrix
 sample.names <- colnames(m)[-1]
-ann.var.names <- c("Ensembl", "Description", "Band", "HGNC", "Chr", "Start", "End")
-names(ann.var) <- ann.var.names
 m.ann <- merge(m, ann.var[,ann.var.names], all.x = T)
 m.ann <- m.ann[,c(ann.var.names, sample.names)]
 
